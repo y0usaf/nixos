a8771ee9ae7703f0adb9e09fe88ebae7956feb95
y0usaf <OA99@Outlook.com>
2025-01-13 03:33:25 -0500

    auto: system update 2025-01-13 03:33:25

--- fabric.nix
+++ fabric.nix
@@ -29,15 +29,17 @@
 
   # Set up virtual environment and install dependencies
   home.activation.fabricSetup = lib.hm.dag.entryAfter ["writeBoundary"] ''
-    $DRY_RUN_CMD mkdir -p $HOME/.config/fabric
+    $DRY_RUN_CMD mkdir -p $HOME/.config/fabric/cache
 
     # Create virtual environment if it doesn't exist
     if [ ! -d "$HOME/.config/fabric/venv" ]; then
+      UV_CACHE_DIR="$HOME/.config/fabric/cache" \
       ${inputs.uv2nix.packages.${pkgs.system}.uv-bin}/bin/uv venv "$HOME/.config/fabric/venv"
 
       # Install required packages
       source "$HOME/.config/fabric/venv/bin/activate"
       export GI_TYPELIB_PATH="${pkgs.gtk3}/lib/girepository-1.0:${pkgs.gobject-introspection}/lib/girepository-1.0"
+      UV_CACHE_DIR="$HOME/.config/fabric/cache" \
       ${inputs.uv2nix.packages.${pkgs.system}.uv-bin}/bin/uv pip install \
         pygobject \
         dbus-python
