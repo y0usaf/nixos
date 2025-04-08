44200e24b7fc8d7de9c934c49f6390c659474f87
y0usaf <OA99@Outlook.com>
2025-04-08 07:47:37 -0400

    Update programs.nix to use host instead of profile

--- modules/system/programs.nix
+++ modules/system/programs.nix
@@ -8,7 +8,7 @@
   config,
   lib,
   pkgs,
-  profile,
+  host,
   inputs,
   ...
 }: {
@@ -20,7 +20,7 @@
     programs = {
       # Conditional configuration for the Hyprland window manager:
       # Only enable if both Wayland and Hyprland are desired features.
-      hyprland = lib.mkIf (profile.cfg.ui.wayland.enable && profile.cfg.ui.hyprland.enable) {
+      hyprland = lib.mkIf (host.cfg.ui.wayland.enable && host.cfg.ui.hyprland.enable) {
         enable = true;
         xwayland.enable = true; # Enable XWayland to support legacy X11 apps.
         # Use the Hyprland package corresponding to the current system.
