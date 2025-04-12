338d45caa18725a088262d97bda16df4009de6ed
y0usaf <OA99@Outlook.com>
2025-04-12 01:48:34 -0400

    fix: separate options declaration to avoid duplication error

---
+++ modules/home/gaming/options.nix
@@ -0,0 +1,20 @@
+# modules/home/gaming/options.nix
+# Defines the gaming module options
+{
+  lib,
+  ...
+}: {
+  options.cfg.programs.gaming = {
+    enable = lib.mkEnableOption "gaming module";
+
+    emulation = {
+      wii-u = {
+        enable = lib.mkEnableOption "Wii U emulation via Cemu";
+      };
+
+      gcn-wii = {
+        enable = lib.mkEnableOption "GameCube and Wii emulation via Dolphin";
+      };
+    };
+  };
+}
