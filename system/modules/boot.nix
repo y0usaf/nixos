c7d10af9731e4b69b208b628ca763375e4d90e3f
y0usaf <OA99@Outlook.com>
2025-04-23 14:16:19 -0400

    fix: update boot module to use hardware.amdgpu and fix hardware.graphics

--- system/modules/boot.nix
+++ system/modules/boot.nix
@@ -41,12 +41,12 @@
           "ashmem_linux"
           "binder_linux"
         ]
-        ++ lib.optionals hostSystem.cfg.core.amdgpu.enable ["amdgpu"];
+        ++ lib.optionals hostSystem.cfg.hardware.amdgpu.enable ["amdgpu"];
       kernel.sysctl = {
         "kernel.unprivileged_userns_clone" = 1; # Allow unprivileged processes to create user namespaces.
       };
       # AMD GPU kernel parameters (conditional)
-      kernelParams = lib.mkIf hostSystem.cfg.core.amdgpu.enable [
+      kernelParams = lib.mkIf hostSystem.cfg.hardware.amdgpu.enable [
         "amdgpu.ppfeaturemask=0xffffffff"
         "amdgpu.dpm=1"
       ];
