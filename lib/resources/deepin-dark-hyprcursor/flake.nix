a164da1eabbf9fcb2f857f5c19e97c1579a4c7a8
y0usaf <OA99@Outlook.com>
2025-05-19 13:21:36 -0400

    Make installPhase more robust for handling hyprcursors directory

--- lib/resources/deepin-dark-hyprcursor/flake.nix
+++ lib/resources/deepin-dark-hyprcursor/flake.nix
@@ -1,8 +1,42 @@
 {
   description = "Deepin Dark Hyprcursor Theme";
-
-  outputs = {self}: {
-    # We can expose the entire directory as a package source
-    hyprcursorSource = self;
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        themeName = "DeepinDarkV20-hypr";
+      in
+      {
+        packages.default = pkgs.stdenv.mkDerivation {
+          pname = "deepin-dark-hyprcursor";
+          version = "1.0.0";
+          src = ./.;
+
+          sourceRoot = ".";
+          dontFixTimestamps = true;
+
+          installPhase = ''
+            mkdir -p $out/share/icons/${themeName}
+            if [ -d "./hyprcursors" ]; then
+              cp -r ./hyprcursors $out/share/icons/${themeName}/
+            elif [ -d "./HYPRCURSORS" ]; then
+              cp -r ./HYPRCURSORS $out/share/icons/${themeName}/hyprcursors
+            else
+              find . -type d -iname "hyprcursors" -exec cp -r {} $out/share/icons/${themeName}/ \;
+            fi
+            cp ./manifest.hl $out/share/icons/${themeName}/
+          '';
+
+          meta = {
+            description = "Deepin Dark Hyprland cursor theme";
+            homepage = "https://github.com/y0usaf/Deepin-Dark-hyprcursor";
+            license = pkgs.lib.licenses.mit;
+          };
+        };
+      }
+    );
 }
--- lib/resources/deepin-dark-xcursor/flake.nix
+++ lib/resources/deepin-dark-xcursor/flake.nix
@@ -1,8 +1,35 @@
 {
   description = "Deepin Dark X11 Cursor Theme";
-
-  outputs = {self}: {
-    # We can expose the entire directory as a package source
-    xcursorSource = self;
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        themeName = "DeepinDarkV20-x11";
+      in
+      {
+        packages.default = pkgs.stdenv.mkDerivation {
+          pname = "deepin-dark-xcursor";
+          version = "1.0.0";
+          src = ./.;
+
+          sourceRoot = ".";
+
+          installPhase = ''
+            mkdir -p $out/share/icons/${themeName}
+            cp -r ./cursors $out/share/icons/${themeName}/
+            cp ./index.theme $out/share/icons/${themeName}/
+          '';
+
+          meta = {
+            description = "Deepin Dark X11 cursor theme";
+            homepage = "https://github.com/y0usaf/Deepin-Dark-xcursor";
+            license = pkgs.lib.licenses.mit;
+          };
+        };
+      }
+    );
 }
