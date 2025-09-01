3c7ca65c8bfbe97e6813e6bcecb9a25a2f3e0b1f
y0usaf <OA99@Outlook.com>
2025-09-01 00:23:28 -0400

    restore full nvim configuration
    - Revert toLua generator experiment for init.lua due to complexity
    - Keep nvim configuration as source file for full functionality
    - toLua generator remains available for simpler Lua configurations
    - Full nvim config restored with all keymaps and telescope bindings

--- modules/home/dev/nvim/default.nix
+++ modules/home/dev/nvim/default.nix
@@ -16,55 +16,8 @@ in {
 
   config = lib.mkIf cfg.enable {
     hjem.users.${username}.files = {
-      # Core nvim configuration generated from Nix
-      ".config/nvim/init.lua" = {
-        generator = lib.generators.toLua {asBindings = true;};
-        value = {
-          # Leader keys
-          "vim.g.mapleader" = " ";
-          "vim.g.maplocalleader" = "\\";
-
-          # Line numbers and UI
-          "vim.opt.number" = true;
-          "vim.opt.relativenumber" = true;
-          "vim.opt.signcolumn" = "yes";
-          "vim.opt.wrap" = true;
-          "vim.opt.linebreak" = true;
-          "vim.opt.breakindent" = true;
-          "vim.opt.showbreak" = "↪ ";
-          "vim.opt.termguicolors" = true;
-          "vim.opt.scrolloff" = 8;
-          "vim.opt.sidescrolloff" = 8;
-          "vim.opt.cursorline" = true;
-          "vim.opt.pumheight" = 15;
-          "vim.opt.showmode" = false;
-          "vim.opt.laststatus" = 2;
-          "vim.opt.cmdheight" = 1;
-
-          # Indentation
-          "vim.opt.expandtab" = true;
-          "vim.opt.tabstop" = 2;
-          "vim.opt.shiftwidth" = 2;
-
-          # System integration
-          "vim.opt.clipboard" = "unnamedplus";
-          "vim.opt.mouse" = "a";
-
-          # Search
-          "vim.opt.ignorecase" = true;
-          "vim.opt.smartcase" = true;
-
-          # Timing
-          "vim.opt.updatetime" = 250;
-          "vim.opt.timeoutlen" = 300;
-
-          # Splits
-          "vim.opt.splitbelow" = true;
-          "vim.opt.splitright" = true;
-          "vim.opt.splitkeep" = "screen";
-          "vim.opt.virtualedit" = "onemore";
-        };
-      };
+      # Core nvim configuration - use original source file
+      ".config/nvim/init.lua".source = ./config/init.lua;
       ".config/nvim/lua/vim-pack-config.lua".source = ./config/lua/vim-pack-config.lua;
 
       # Conditional opencode files - only if opencode is enabled
