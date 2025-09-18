17b159f3724b1ba1281d2bcd6f573011093d47e4
y0usaf <OA99@Outlook.com>
2025-09-18 13:42:28 -0400

    Improve environment variables to inherit from user session
    - Use extraInit with fallback defaults instead of hardcoded values
    - XDG_CURRENT_DESKTOP inherits from session, defaults to "niri"
    - WAYLAND_DISPLAY inherits from session, defaults to "wayland-1"
    - More flexible approach that adapts to different desktop environments
    - Maintains portal integration while avoiding hardcoded assumptions

--- modules/system/core/environment.nix
+++ modules/system/core/environment.nix
@@ -1,8 +1,10 @@
 _: {
   config = {
-    environment.sessionVariables = {
-      XDG_CURRENT_DESKTOP = "niri";
-      WAYLAND_DISPLAY = "wayland-1";
-    };
+    # Set up variables to be inherited from the user session
+    environment.extraInit = ''
+      # Inherit portal integration variables from active session
+      export XDG_CURRENT_DESKTOP=''${XDG_CURRENT_DESKTOP:-"niri"}
+      export WAYLAND_DISPLAY=''${WAYLAND_DISPLAY:-"wayland-1"}
+    '';
   };
 }
