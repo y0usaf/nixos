570a13537afde56462c815e944683a806281240e
y0usaf <OA99@Outlook.com>
2025-09-21 22:15:37 -0400

    Fix Clair Obscur path

--- modules/home/gaming/clair-obscur/plugin.nix
+++ modules/home/gaming/clair-obscur/plugin.nix
@@ -24,13 +24,13 @@ in {
   config = lib.mkIf cfg.enable {
     usr.files = {
       # Install the ASI plugin
-      ".local/share/Steam/steamapps/common/Clair Obscur Expedition 33/ClairObscurFix.asi" = {
+      ".local/share/Steam/steamapps/common/Expedition 33/ClairObscurFix.asi" = {
         clobber = true;
         source = "${clairobscurfix}/ClairObscurFix.asi";
       };
 
       # Install the configuration file
-      ".local/share/Steam/steamapps/common/Clair Obscur Expedition 33/ClairObscurFix.ini" = {
+      ".local/share/Steam/steamapps/common/Expedition 33/ClairObscurFix.ini" = {
         clobber = true;
         generator = lib.generators.toINI {};
         value = {
