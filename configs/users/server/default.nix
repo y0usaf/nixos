aa25e3cfd8accff7d5528a9608016a1ef482e4c4
y0usaf <OA99@Outlook.com>
2025-10-06 02:58:40 -0400

    feat: add server device ID to Syncthing configs
    - Add server device (GY3T3SL-3JOOX3I-2SE72PF-V6ZSTIE-QI4EIYK-OBL6IDV-4IWLDDG-VM2ATAG)
    - Include server in Tokens folder sync on all devices
    - Server can now sync with desktop, laptop, and phone

--- configs/users/server/default.nix
+++ configs/users/server/default.nix
@@ -98,6 +98,9 @@
           laptop = {
             id = "EAHAPON-XKBJVGI-44SGTXR-WU6BF5U-WZKHJXS-7QNTBHQ-D4ICOVA-I346HQ7";
           };
+          server = {
+            id = "GY3T3SL-3JOOX3I-2SE72PF-V6ZSTIE-QI4EIYK-OBL6IDV-4IWLDDG-VM2ATAG";
+          };
           phone = {
             id = "JYAIN4T-MXQYDAP-2M6CSKX-KKRYVJC-5GMSRYP-LSZRRRV-QSOWY7W-YNQGOAC";
             compression = "never";
@@ -110,7 +113,7 @@
             id = "bv79n-fh4kx";
             label = "Tokens";
             path = "~/Tokens";
-            devices = ["desktop" "laptop" "phone"];
+            devices = ["desktop" "laptop" "server" "phone"];
           };
         };
       };
