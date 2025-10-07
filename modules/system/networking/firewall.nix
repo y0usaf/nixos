6c36b449ba108ebada6cee6b274a1d1f45b1c397
y0usaf <OA99@Outlook.com>
2025-10-07 02:00:08 -0400

    fix: add Forgejo and Syncthing ports to firewall

--- modules/system/networking/firewall.nix
+++ modules/system/networking/firewall.nix
@@ -3,9 +3,14 @@ _: {
     networking.firewall = {
       enable = true;
       allowedTCPPorts = [
-        25565
+        22 # SSH
+        3000 # Forgejo
+        22000 # Syncthing
+        25565 # Minecraft
       ];
       allowedUDPPorts = [
+        22000 # Syncthing
+        21027 # Syncthing discovery
       ];
     };
   };
