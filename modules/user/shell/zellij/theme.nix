3b52353e563107ce59cb424240f8d214efbca5f5
y0usaf <OA99@Outlook.com>
2025-10-12 09:13:50 -0400

    feat: convert neon theme to modern Zellij palette format
    Converted theme from legacy format (basic colors) to modern format
    with full palette components (ribbon_unselected, text_unselected, etc).
    
    This allows zjstatus-hints plugin to properly pick up neon colors:
    - ribbon_unselected.background = neon green (#00ff64)
    - ribbon_unselected.base = black text on bright background
    - Hints should now display with proper neon styling
    
    Plugin will also show correct format: "Ctrl  p t n" (modifier once,
    then individual keys) matching the showcase video.

--- modules/user/shell/zellij/theme.nix
+++ modules/user/shell/zellij/theme.nix
@@ -7,17 +7,110 @@
 
   neonTheme = {
     themes.neon = {
-      bg = "#0f0f0f";
-      fg = "#b4b4b4";
-      red = "#ff0064";
-      green = "#00ff64";
-      blue = "#00c8ff";
-      yellow = "#ff6400";
-      magenta = "#c800ff";
-      orange = "#ff6400";
-      cyan = "#00ff96";
-      black = "#000000";
-      white = "#ffffff";
+      text_unselected = {
+        base = "180 180 180"; # #b4b4b4
+        background = "15 15 15"; # #0f0f0f
+        emphasis_0 = "255 100 0"; # orange
+        emphasis_1 = "0 255 150"; # cyan
+        emphasis_2 = "0 255 100"; # green
+        emphasis_3 = "255 0 100"; # red
+      };
+      text_selected = {
+        base = "180 180 180";
+        background = "31 31 31"; # darker grey
+        emphasis_0 = "255 100 0";
+        emphasis_1 = "0 255 150";
+        emphasis_2 = "0 255 100";
+        emphasis_3 = "255 0 100";
+      };
+      ribbon_unselected = {
+        base = "0 0 0"; # black text on bright bg
+        background = "0 255 100"; # neon green
+        emphasis_0 = "255 0 100";
+        emphasis_1 = "0 200 255";
+        emphasis_2 = "200 0 255";
+        emphasis_3 = "255 100 0";
+      };
+      ribbon_selected = {
+        base = "0 0 0";
+        background = "0 255 150"; # neon cyan for active
+        emphasis_0 = "255 0 100";
+        emphasis_1 = "0 200 255";
+        emphasis_2 = "200 0 255";
+        emphasis_3 = "255 100 0";
+      };
+      table_title = {
+        base = "0 255 100";
+        background = "15 15 15";
+        emphasis_0 = "255 100 0";
+        emphasis_1 = "0 255 150";
+        emphasis_2 = "200 0 255";
+        emphasis_3 = "0 200 255";
+      };
+      table_cell_unselected = {
+        base = "180 180 180";
+        background = "15 15 15";
+        emphasis_0 = "255 100 0";
+        emphasis_1 = "0 255 150";
+        emphasis_2 = "0 255 100";
+        emphasis_3 = "255 0 100";
+      };
+      table_cell_selected = {
+        base = "180 180 180";
+        background = "31 31 31";
+        emphasis_0 = "255 100 0";
+        emphasis_1 = "0 255 150";
+        emphasis_2 = "0 255 100";
+        emphasis_3 = "255 0 100";
+      };
+      list_unselected = {
+        base = "180 180 180";
+        background = "15 15 15";
+        emphasis_0 = "255 100 0";
+        emphasis_1 = "0 255 150";
+        emphasis_2 = "0 255 100";
+        emphasis_3 = "255 0 100";
+      };
+      list_selected = {
+        base = "180 180 180";
+        background = "31 31 31";
+        emphasis_0 = "255 100 0";
+        emphasis_1 = "0 255 150";
+        emphasis_2 = "0 255 100";
+        emphasis_3 = "255 0 100";
+      };
+      frame_selected = {
+        base = "0 255 100"; # neon green border
+        background = "15 15 15";
+        emphasis_0 = "255 100 0";
+        emphasis_1 = "0 255 150";
+        emphasis_2 = "200 0 255";
+        emphasis_3 = "0 200 255";
+      };
+      frame_highlight = {
+        base = "0 200 255"; # neon blue highlight
+        background = "15 15 15";
+        emphasis_0 = "255 100 0";
+        emphasis_1 = "200 0 255";
+        emphasis_2 = "0 255 100";
+        emphasis_3 = "255 0 100";
+      };
+      exit_code_success = {
+        base = "0 255 100";
+        background = "15 15 15";
+        emphasis_0 = "0 255 150";
+        emphasis_1 = "0 200 255";
+        emphasis_2 = "200 0 255";
+        emphasis_3 = "255 100 0";
+      };
+      exit_code_error = {
+        base = "255 0 100";
+        background = "15 15 15";
+        emphasis_0 = "255 100 0";
+        emphasis_1 = "0 0 0";
+        emphasis_2 = "0 0 0";
+        emphasis_3 = "0 0 0";
+      };
     };
   };
 
