a38fd4447fc5be753c552893ea5175f0eba4bed8
y0usaf <OA99@Outlook.com>
2025-10-20 02:00:41 -0400

    Nvim changes

--- modules/user/dev/nvim/packages.nix
+++ modules/user/dev/nvim/packages.nix
@@ -17,7 +17,65 @@
           tree-sitter-markdown
           tree-sitter-json
         ]))
-      pkgs.neovim
+      # Neovim with plugins
+      (pkgs.neovim.override {
+        configure = {
+          packages.myNeovimPackage = with pkgs.vimPlugins; {
+            start = [
+              # Core dependencies
+              plenary-nvim
+              nvim-web-devicons
+              nui-nvim
+
+              # File navigation
+              neo-tree-nvim
+
+              # Telescope
+              telescope-nvim
+              telescope-fzf-native-nvim
+
+              # LSP and Completion
+              nvim-lspconfig
+              nvim-cmp
+              cmp-nvim-lsp
+              cmp-buffer
+              cmp-path
+              luasnip
+              cmp_luasnip
+              lspkind-nvim
+              lsp_lines-nvim
+
+              # Treesitter
+              nvim-treesitter
+              nvim-treesitter-textobjects
+
+              # Utilities
+              comment-nvim
+              nvim-autopairs
+              nvim-ufo
+              promise-async
+              fidget-nvim
+              vim-illuminate
+              twilight-nvim
+              toggleterm-nvim
+
+              # Theme & UI
+              cyberdream-nvim
+              dropbar-nvim
+              dressing-nvim
+              windows-nvim
+              lualine-nvim
+              gitsigns-nvim
+              indent-blankline-nvim
+              flash-nvim
+              mini-nvim
+
+              # Specialized
+              wilder-nvim
+            ];
+          };
+        };
+      })
       pkgs.lua-language-server
       pkgs.nil
       pkgs.pyright
