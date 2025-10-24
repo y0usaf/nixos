7a942adea8d6dc10287a465e63bd30f80a659e1c
y0usaf <OA99@Outlook.com>
2025-10-23 22:03:05 -0400

    feat: combine kernel and version into single Kernel column

--- nixos/user/shell/nushell/config.nix
+++ nixos/user/shell/nushell/config.nix
@@ -90,7 +90,7 @@ in {
                 let kr = ($row | get "kernel-release")
                 let os = ($row | get "operating-system")
                 let m = ($row | get machine)
-                {User: ($env.USER), Host: (hostname), Kernel: $k, Version: $kr, OS: $os, Machine: $m}
+                {User: ($env.USER), Host: (hostname), Kernel: $"$k $kr", OS: $os, Machine: $m}
               }
             '';
             clobber = true;
