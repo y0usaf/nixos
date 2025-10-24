fa4d97a2302a22c41d105c4dc10a18b371c5d5fe
y0usaf <OA99@Outlook.com>
2025-10-23 21:18:48 -0400

    feat: rename nushell startup table columns for better readability

--- nixos/user/shell/nushell/config.nix
+++ nixos/user/shell/nushell/config.nix
@@ -85,7 +85,14 @@ in {
               $env.config.completions.algorithm = "prefix"
 
               $env.config.show_banner = false
-              uname | reject kernel-version | insert User $env.USER | insert Host (hostname)
+              uname | reject kernel-version | each { |row|
+                let k = ($row | get "kernel-name")
+                let n = ($row | get nodename)
+                let kr = ($row | get "kernel-release")
+                let os = ($row | get "operating-system")
+                let m = ($row | get machine)
+                {Kernel: $k, Nodename: $n, "Kernel-Release": $kr, "Operating-System": $os, Machine: $m, User: ($env.USER), Host: (hostname)}
+              }
             '';
             clobber = true;
           };
