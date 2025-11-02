1b7f400cefed581454ba68e38863b821b58f97f5
y0usaf <OA99@Outlook.com>
2025-11-02 11:58:18 -0500

    refactor: Use lib/nvim modules for neovim configuration
    Import and utilize the shared nvim configuration modules (plugins, settings, keymaps)
    from lib/nvim to eliminate duplication and establish a single source of truth for
    both NixOS and Darwin systems. This enables seamless configuration sharing across
    systems while maintaining readability.
    
    - Refactored nvim/init.lua to import from lib/nvim
    - Removed 169 lines of duplicated code from default.nix
    - Established unified configuration module that both systems can use

--- nixos/user/dev/nvim/default.nix
+++ nixos/user/dev/nvim/default.nix
@@ -2,7 +2,9 @@
   config,
   lib,
   ...
-}: {
+}: let
+  nvim = import ../../../../lib/nvim;
+in {
   imports = [
     ./neovide.nix
     ./options.nix
@@ -18,103 +20,10 @@
 
         -- Load plugins
         vim.pack.add({
-          -- Core dependencies
-          "https://github.com/nvim-lua/plenary.nvim",
-          "https://github.com/nvim-tree/nvim-web-devicons",
-          "https://github.com/MunifTanjim/nui.nvim",
-
-          -- File navigation
-          "https://github.com/nvim-neo-tree/neo-tree.nvim",
-
-          -- Telescope
-          "https://github.com/nvim-telescope/telescope.nvim",
-          "https://github.com/nvim-telescope/telescope-fzf-native.nvim",
-
-          -- LSP and Completion
-          "https://github.com/neovim/nvim-lspconfig",
-          "https://github.com/hrsh7th/nvim-cmp",
-          "https://github.com/hrsh7th/cmp-nvim-lsp",
-          "https://github.com/hrsh7th/cmp-buffer",
-          "https://github.com/hrsh7th/cmp-path",
-          "https://github.com/L3MON4D3/LuaSnip",
-          "https://github.com/saadparwaiz1/cmp_luasnip",
-          "https://github.com/onsails/lspkind.nvim",
-          "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
-
-          -- Treesitter
-          "https://github.com/nvim-treesitter/nvim-treesitter",
-
-          -- Utilities
-          "https://github.com/numToStr/Comment.nvim",
-          "https://github.com/windwp/nvim-autopairs",
-          "https://github.com/kevinhwang91/nvim-ufo",
-          "https://github.com/kevinhwang91/promise-async",
-          "https://github.com/j-hui/fidget.nvim",
-          "https://github.com/RRethy/vim-illuminate",
-          "https://github.com/folke/twilight.nvim",
-          "https://github.com/akinsho/toggleterm.nvim",
-
-          -- Theme & UI
-          "https://github.com/scottmckendry/cyberdream.nvim",
-          "https://github.com/Bekaboo/dropbar.nvim",
-          "https://github.com/stevearc/dressing.nvim",
-          "https://github.com/anuvyklack/windows.nvim",
-          "https://github.com/anuvyklack/middleclass",
-          "https://github.com/nvim-lualine/lualine.nvim",
-          "https://github.com/lewis6991/gitsigns.nvim",
-          "https://github.com/lukas-reineke/indent-blankline.nvim",
-          "https://github.com/folke/flash.nvim",
-          "https://github.com/echasnovski/mini.hipatterns",
-
-          -- Specialized
-          "https://github.com/kawre/leetcode.nvim",
-          "https://github.com/gelguy/wilder.nvim",
+          ${lib.concatMapStringsSep ",\n          " (p: "\"${p}\"") nvim.plugins}
         })
 
-        -- Line numbers and UI
-        vim.opt.number = true
-        vim.opt.relativenumber = true
-        vim.opt.signcolumn = "yes"
-        vim.opt.wrap = true
-        vim.opt.linebreak = true
-        vim.opt.breakindent = true
-        vim.opt.showbreak = "↪ "
-        vim.opt.termguicolors = true
-        vim.opt.scrolloff = 8
-        vim.opt.sidescrolloff = 8
-        vim.opt.cursorline = true
-        vim.opt.pumheight = 15
-        vim.opt.showmode = false
-        vim.opt.laststatus = 2
-        vim.opt.cmdheight = 1
-
-        -- Indentation
-        vim.opt.expandtab = true
-        vim.opt.tabstop = 2
-        vim.opt.shiftwidth = 2
-
-        -- System integration
-        vim.opt.clipboard = "unnamedplus"
-        vim.opt.mouse = "a"
-
-        -- Search
-        vim.opt.ignorecase = true
-        vim.opt.smartcase = true
-
-        -- Timing
-        vim.opt.updatetime = 250
-        vim.opt.timeoutlen = 300
-
-        -- Splits
-        vim.opt.splitbelow = true
-        vim.opt.splitright = true
-        vim.opt.splitkeep = "screen"
-        vim.opt.virtualedit = "onemore"
-
-        -- Additional UI options
-        vim.opt.shortmess:append({ W = true, I = true, c = true, C = true })
-        vim.opt.fillchars = { eob = " ", fold = " ", foldsep = " ", diff = "/" }
-        vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+        ${nvim.settings}
 
         -- Disable netrw for neo-tree
         vim.g.loaded_netrw = 1
@@ -367,79 +276,7 @@
         require("dressing").setup({})
         require("windows").setup({})
 
-        -- Keymaps setup
-        local keymap = vim.keymap.set
-        local builtin = require("telescope.builtin")
-
-        -- Telescope keymaps
-        keymap("n", "<leader>ff", builtin.find_files, { desc = "Find files" })
-        keymap("n", "<leader>fg", builtin.live_grep, { desc = "Live grep" })
-        keymap("n", "<leader>fb", builtin.buffers, { desc = "Find buffers" })
-        keymap("n", "<leader>fr", builtin.oldfiles, { desc = "Recent files" })
-        keymap("n", "<leader>fh", builtin.help_tags, { desc = "Help tags" })
-        keymap("n", "<leader>fo", builtin.git_status, { desc = "Git status" })
-
-        -- File navigation with neo-tree
-        keymap("n", "-", "<cmd>Neotree toggle<cr>", { desc = "Toggle neo-tree" })
-        keymap("n", "<leader>-", "<cmd>Neotree focus<cr>", { desc = "Focus neo-tree" })
-        keymap("n", "<leader>e", "<cmd>Neotree reveal<cr>", { desc = "Reveal file in neo-tree" })
-
-        -- Buffer navigation
-        keymap("n", "<S-h>", "<cmd>bprevious<cr>", { desc = "Previous buffer" })
-        keymap("n", "<S-l>", "<cmd>bnext<cr>", { desc = "Next buffer" })
-        keymap("n", "<leader>bd", "<cmd>bdelete<cr>", { desc = "Delete buffer" })
-
-        -- Window navigation
-        keymap("n", "<C-h>", "<C-w>h", { desc = "Go to left window" })
-        keymap("n", "<C-j>", "<C-w>j", { desc = "Go to lower window" })
-        keymap("n", "<C-k>", "<C-w>k", { desc = "Go to upper window" })
-        keymap("n", "<C-l>", "<C-w>l", { desc = "Go to right window" })
-
-        -- Utility keymaps
-        keymap("n", "<leader>ut", "<cmd>Twilight<cr>", { desc = "Toggle twilight" })
-        keymap("n", "<C-\\", "<cmd>ToggleTerm<cr>", { desc = "Toggle terminal" })
-        keymap("t", "<C-\\", "<cmd>ToggleTerm<cr>", { desc = "Toggle terminal" })
-
-        -- UFO fold keymaps
-        keymap("n", "zR", function() require("ufo").openAllFolds() end, { desc = "Open all folds" })
-        keymap("n", "zM", function() require("ufo").closeAllFolds() end, { desc = "Close all folds" })
-        keymap("n", "zr", function() require("ufo").openFoldsExceptKinds() end, { desc = "Open folds except kinds" })
-        keymap("n", "zm", function() require("ufo").closeFoldsWith() end, { desc = "Close folds with" })
-        keymap("n", "zp", function() require("ufo").peekFoldedLinesUnderCursor() end, { desc = "Peek folded lines" })
-
-        -- Diagnostics
-        keymap("n", "<leader>xx", builtin.diagnostics, { desc = "Diagnostics" })
-        keymap("n", "<leader>xd", function() builtin.diagnostics({ bufnr = 0 }) end, { desc = "Buffer diagnostics" })
-
-        -- Leetcode
-        keymap("n", "<leader>lq", "<cmd>Leet<cr>", { desc = "Leetcode menu" })
-        keymap("n", "<leader>ll", "<cmd>Leet list<cr>", { desc = "Leetcode list" })
-        keymap("n", "<leader>lt", "<cmd>Leet test<cr>", { desc = "Leetcode test" })
-        keymap("n", "<leader>ls", "<cmd>Leet submit<cr>", { desc = "Leetcode submit" })
-        keymap("n", "<leader>ln", "<cmd>messages<cr>", { desc = "View messages" })
-
-        -- Basic keymaps
-        keymap("n", "<leader>w", "<cmd>w<cr>", { desc = "Save file" })
-        keymap("n", "<leader>q", "<cmd>q<cr>", { desc = "Quit" })
-        keymap("n", "<leader>/", function() require("Comment.api").toggle.linewise.current() end, { desc = "Toggle comment" })
-        keymap("v", "<leader>/", "<esc><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<cr>", { desc = "Toggle comment" })
-
-        -- Better movement
-        keymap({ "n", "x" }, "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
-        keymap({ "n", "x" }, "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
-
-        -- Move lines
-        keymap("n", "<A-j>", "<cmd>m .+1<cr>==", { desc = "Move down" })
-        keymap("n", "<A-k>", "<cmd>m .-2<cr>==", { desc = "Move up" })
-        keymap("i", "<A-j>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move down" })
-        keymap("i", "<A-k>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move up" })
-        keymap("v", "<A-j>", ":m '>+1<cr>gv=gv", { desc = "Move down" })
-        keymap("v", "<A-k>", ":m '<-2<cr>gv=gv", { desc = "Move up" })
-
-        -- Indenting
-        keymap("v", "<", "<gv")
-        keymap("v", ">", ">gv")
-        keymap("n", "<Esc>", "<cmd>nohlsearch<CR>")
+        ${nvim.keymaps}
       '';
     };
   };
