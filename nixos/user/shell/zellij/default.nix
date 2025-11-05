305327f265f2fa93f10b065d5c4b7de62a08a02d
y0usaf <OA99@Outlook.com>
2025-11-05 13:59:38 -0500

    refactor: Split nixos/user/shell/zellij into default and config
    - default.nix: just imports (cleaner entry point)
    - config.nix: all file writing logic
    Follows standard pattern of default=imports, config=implementation

--- nixos/user/shell/zellij/default.nix
+++ nixos/user/shell/zellij/default.nix
@@ -1,48 +1,6 @@
-{
-  config,
-  lib,
-  pkgs,
-  genLib,
-  ...
-}: {
+_: {
   imports = [
     ../../../../lib/shell/zellij/default.nix
+    ./config.nix
   ];
-
-  config = lib.mkIf config.user.shell.zellij.enable {
-    environment.systemPackages = [
-      pkgs.zellij
-    ];
-
-    usr.files =
-      {
-        ".config/zellij/config.kdl" = {
-          clobber = false;
-          text =
-            genLib.toKDL (
-              (import ../../../../lib/shell/zellij/config.nix {inherit lib;}).mkKdlAttrs {
-                zjstatusEnabled = config.user.shell.zellij.zjstatus.enable;
-                userSettings = config.user.shell.zellij.settings;
-              }
-            )
-            + "\n\n// Using default keybindings for now\n"
-            + (import ../../../../lib/shell/zellij/config.nix {inherit lib;}).mkPluginsString {
-              zjstatusEnabled = config.user.shell.zellij.zjstatus.enable;
-            }
-            + genLib.toKDL (import ../../../../lib/shell/zellij/config.nix {inherit lib;}).theme;
-        };
-      }
-      // lib.optionalAttrs (config.user.shell.zellij.autoStart && config.user.shell.zsh.enable) {
-        ".config/zsh/zellij.zsh" = {
-          clobber = true;
-          text = (import ../../../../lib/shell/zellij/config.nix {inherit lib;}).shellIntegration;
-        };
-      }
-      // lib.optionalAttrs (config.user.shell.zellij.enable && config.user.shell.zellij.zjstatus.enable) {
-        ".config/zellij/layouts/zjstatus.kdl" = {
-          clobber = true;
-          text = config.user.shell.zellij.zjstatus.layout;
-        };
-      };
-  };
 }
