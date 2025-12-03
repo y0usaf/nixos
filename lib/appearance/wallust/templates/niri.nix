1da1fbc45c00ed2e9dbe95af1b70f7bdd6b4175e
y0usaf <OA99@Outlook.com>
2025-12-03 12:54:40 -0500

    refactor(wallust): unify niri border colors template

--- lib/appearance/wallust/templates/niri.nix
+++ lib/appearance/wallust/templates/niri.nix
@@ -1,15 +1,15 @@
 # Niri border colors template (included via niri's include directive)
-# Uses cursor color as accent (cursor == accent in our colorschemes), color7 for inactive
+# Uses cursor color as accent (cursor == accent in our colorschemes), color8 for inactive
 ''
   layout {
     border {
       on
       active-color "{{ cursor }}"
-      inactive-color "{{ color7 }}"
+      inactive-color "{{ color8 }}"
     }
     tab-indicator {
       active-color "{{ cursor }}"
-      inactive-color "{{ color7 }}"
+      inactive-color "{{ color8 }}"
     }
   }
 ''
