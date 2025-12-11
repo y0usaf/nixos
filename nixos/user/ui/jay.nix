60f6491fb095efcd90c5fe9e1d6a5b00887ae0ee
y0usaf <OA99@Outlook.com>
2025-12-11 12:06:36 -0500

    Make Codex MCP hook less prescriptive - suggest review usage instead of mandatory on every response

--- nixos/user/ui/jay.nix
+++ nixos/user/ui/jay.nix
@@ -1,9 +1,21 @@
 {
+  pkgs,
   flakeInputs,
   system,
   ...
-}: {
-  environment.systemPackages = [
-    flakeInputs.jay.packages.${system}.jay
-  ];
+}: let
+  jayWithLibs = pkgs.symlinkJoin {
+    name = "jay-wrapped";
+    paths = [flakeInputs.jay.packages.${system}.jay];
+    buildInputs = [pkgs.makeWrapper];
+    postBuild = ''
+      wrapProgram $out/bin/jay \
+        --prefix LD_LIBRARY_PATH : ${pkgs.lib.makeLibraryPath [
+        pkgs.vulkan-loader
+        pkgs.libGL
+      ]}
+    '';
+  };
+in {
+  environment.systemPackages = [jayWithLibs];
 }
