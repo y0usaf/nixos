1238acff0047af5fcad68b6b6a10b18ec5d8c2ad
y0usaf <OA99@Outlook.com>
2025-12-12 12:17:55 -0500

    Add NPM XDG compliance environment variables
    Configure npm to use XDG directories:
    - Cache in $XDG_CACHE_HOME/npm
    - Config in $XDG_CONFIG_HOME/npm
    - Temp files in $XDG_RUNTIME_DIR/npm
    
    Migrated existing ~/.npmrc and ~/.npm to new XDG locations.
    Verified with xdg-ninja - npm compliance now complete.

--- nixos/user/shell/zsh/xdg-env.nix
+++ nixos/user/shell/zsh/xdg-env.nix
@@ -49,6 +49,10 @@
           export CUDA_CACHE_PATH="$XDG_CACHE_HOME/nv"
           export GRADLE_USER_HOME="$XDG_DATA_HOME/gradle"
           export GTK2_RC_FILES="$XDG_CONFIG_HOME/gtk-2.0/gtkrc"
+          export NPM_CONFIG_USERCONFIG="$XDG_CONFIG_HOME/npm/npmrc"
+          export NPM_CONFIG_CACHE="$XDG_CACHE_HOME/npm"
+          export NPM_CONFIG_TMP="$XDG_RUNTIME_DIR/npm"
+          export NPM_CONFIG_INIT_MODULE="$XDG_CONFIG_HOME/npm/config/npm-init.js"
         '';
       };
 
