5f16e6d0c6284d22c869e53f38560525181231d1
y0usaf <OA99@Outlook.com>
2026-03-14 02:24:45 -0400

    discord: consolidate visual-refresh CSS into single theme file
    Merge 5 separate visual-refresh-hide-*.css files into one
    visual-refresh.css file, reducing file sprawl and simplifying
    CSS imports in discord settings.

--- nixos/user/programs/discord/vencord/visual-refresh.nix
+++ nixos/user/programs/discord/vencord/visual-refresh.nix
@@ -9,7 +9,7 @@
       || (config.user.programs.discord.canary.enable or false)
     ) {
       hjem.users."${config.user.name}".files = {
-        ".config/Vencord/themes/visual-refresh-hide-1.css".text = ''
+        ".config/Vencord/themes/visual-refresh.css".text = ''
           /* Hide the Visual Refresh title bar */
           .visual-refresh {
             --custom-app-top-bar-height: 0px !important;
@@ -17,33 +17,25 @@
               display: none;
             }
           }
-        '';
 
-        ".config/Vencord/themes/visual-refresh-hide-2.css".text = ''
           /* Adjust guild list top offset after hiding title bar */
           .visual-refresh {
             ul[data-list-id="guildsnav"] > div.itemsContainer_ef3116 {
               margin-top: 8px;
             }
           }
-        '';
 
-        ".config/Vencord/themes/visual-refresh-hide-3.css".text = ''
           /* Make "Read All" vencord button text smaller */
           button.vc-ranb-button {
             font-size: 9.5pt;
             font-weight: normal;
           }
-        '';
 
-        ".config/Vencord/themes/visual-refresh-hide-4.css".text = ''
           /* Hide Discover button */
           div[data-list-item-id="guildsnav___guild-discover-button"] {
             display: none !important;
           }
-        '';
 
-        ".config/Vencord/themes/visual-refresh-hide-5.css".text = ''
           /* Hide the buttons next to mute and deafen */
           div[class^=buttons__] {
             gap: 2px;
