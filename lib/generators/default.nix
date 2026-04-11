021b8e3f012553fa3338360e748c24b3daf7a126
y0usaf <OA99@Outlook.com>
2026-04-11 00:32:34 -0400

    refactor: move generators from lib/ to modules/core/system/generators
    Relocate custom format generators (toHyprconf, toKDL, toNiriconf, toTOML)
    from lib/generators/ to modules/core/system/generators/.
    
    Files renamed to .nixlib extension to avoid auto-import.
    New generators.nix module exposes them via config.lib.generators.

--- lib/generators/default.nix
+++
@@ -1,9 +0,0 @@
-{
-  lib,
-  pkgs,
-}: {
-  inherit ((import ./toHyprconf.nix lib)) toHyprconf;
-  inherit ((import ./toNiriconf.nix lib)) toNiriconf;
-  inherit ((import ./toKDL.nix {inherit lib;})) toKDL;
-  inherit ((import ./toTOML.nix {inherit lib pkgs;})) toTOML;
-}
