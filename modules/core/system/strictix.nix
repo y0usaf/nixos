1c259ce75174c33c2a5c874af97d60b61ae2554c
y0usaf <OA99@Outlook.com>
2026-04-25 19:06:58 -0400

    strictix: enable focused lint rules

--- modules/core/system/strictix.nix
+++ modules/core/system/strictix.nix
@@ -1,4 +1,5 @@
 {
+  config,
   pkgs,
   flakeInputs,
   ...
@@ -6,4 +7,14 @@
   environment.systemPackages = [
     flakeInputs.strictix.packages."${pkgs.stdenv.hostPlatform.system}".default
   ];
+
+  bayt.users."${config.user.name}".files.".config/strictix/config.toml" = {
+    source = (pkgs.formats.toml {}).generate "strictix-config" {
+      enabled = [
+        "single_use_let"
+        "unused_pattern_param"
+      ];
+      lints.unused_pattern_param.remove_ellipsis = true;
+    };
+  };
 }
