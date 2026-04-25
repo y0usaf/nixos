354e6c80c6af7bc3454e78498ced10dfa9d8b9e8
y0usaf <OA99@Outlook.com>
2026-04-25 12:19:04 -0400

    refactor(recursive-import): reuse path predicate

--- recursivelyImport.nix
+++ recursivelyImport.nix
@@ -2,16 +2,18 @@
   hasSuffix,
   listFilesRecursive,
 }: let
+  isPath = elem: builtins.isPath elem;
+
   # Only `.nix` files participate in the recursive module graph.
   # Non-module helpers should be imported explicitly.
   expandIfFolder = elem:
-    if !builtins.isPath elem || builtins.readFileType elem != "directory"
+    if !isPath elem || builtins.readFileType elem != "directory"
     then [elem]
     else listFilesRecursive elem;
 in
   list:
     builtins.filter
     (elem:
-      !builtins.isPath elem
+      !isPath elem
       || hasSuffix ".nix" (toString elem))
     (builtins.concatMap expandIfFolder list)
