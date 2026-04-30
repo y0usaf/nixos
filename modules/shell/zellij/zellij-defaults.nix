a199fba2276624d0f4872bb9ed8727340d96fc80
y0usaf <OA99@Outlook.com>
2026-04-29 22:36:56 -0400

    style(zellij): update status bar colors

--- modules/shell/zellij/zellij-defaults.nix
+++ modules/shell/zellij/zellij-defaults.nix
@@ -7,41 +7,41 @@
             ${''
         plugin location="https://github.com/dj95/zjstatus/releases/download/v0.21.1/zjstatus.wasm" {
           format_left   ""
-          format_center "#[bg=yellow,fg=black,bold] [{session}] #[bg=reset,fg=reset] {mode} {tabs} #[bg=cyan,fg=black,bold] {datetime}"
+          format_center "#[fg=#e0e0ff,bold] [{session}] #[fg=#00ffff,bold] {mode} #[fg=#e0e0ff] {tabs} #[fg=#00ffff,bold] {datetime}"
           format_right  ""
-          format_space  ""
+          format_space  " "
 
-          session "{name}"
+          session "#[fg=#e0e0ff]{name}"
           format_hide_on_overlength "true"
           format_precedence "lrc"
 
           border_enabled  "false"
           hide_frame_for_single_pane "false"
 
-          mode_normal        "#[bg=blue,fg=black,bold] [NORMAL] "
-          mode_locked        "#[bg=red,fg=white,bold] [LOCKED] "
-          mode_resize        "#[bg=yellow,fg=black,bold] [RESIZE] "
-          mode_pane          "#[bg=green,fg=black,bold] [PANE] "
-          mode_tab           "#[bg=yellow,fg=black,bold] [TAB] "
-          mode_scroll        "#[bg=magenta,fg=white,bold] [SCROLL] "
-          mode_enter_search  "#[bg=magenta,fg=white,bold] [SEARCH] "
-          mode_search        "#[bg=magenta,fg=white,bold] [SEARCH] "
-          mode_rename_tab    "#[bg=yellow,fg=black,bold] [RENAME] "
-          mode_rename_pane   "#[bg=green,fg=black,bold] [RENAME] "
-          mode_session       "#[bg=magenta,fg=white,bold] [SESSION] "
-          mode_move          "#[bg=yellow,fg=black,bold] [MOVE] "
-          mode_prompt        "#[bg=magenta,fg=white,bold] [PROMPT] "
-          mode_tmux          "#[bg=red,fg=white,bold] [TMUX] "
+          mode_normal        "#[fg=#00ffff,bold] [NORMAL] "
+          mode_locked        "#[fg=#ff0055,bold] [LOCKED] "
+          mode_resize        "#[fg=#ffcc00,bold] [RESIZE] "
+          mode_pane          "#[fg=#00ff99,bold] [PANE] "
+          mode_tab           "#[fg=#ffcc00,bold] [TAB] "
+          mode_scroll        "#[fg=#ff00ff,bold] [SCROLL] "
+          mode_enter_search  "#[fg=#ff00ff,bold] [SEARCH] "
+          mode_search        "#[fg=#ff00ff,bold] [SEARCH] "
+          mode_rename_tab    "#[fg=#ffcc00,bold] [RENAME] "
+          mode_rename_pane   "#[fg=#00ff99,bold] [RENAME] "
+          mode_session       "#[fg=#ff00ff,bold] [SESSION] "
+          mode_move          "#[fg=#ffcc00,bold] [MOVE] "
+          mode_prompt        "#[fg=#ff00ff,bold] [PROMPT] "
+          mode_tmux          "#[fg=#ff0055,bold] [TMUX] "
 
-          tab_normal              "#[bg=bright_black,fg=black] [{name}] {floating_indicator}"
-          tab_normal_fullscreen   "#[bg=bright_black,fg=black] [{name}] [] {floating_indicator}"
-          tab_normal_sync         "#[bg=bright_black,fg=black] [{name}] <> {floating_indicator}"
-          tab_active              "#[bg=cyan,fg=black,bold] [{name}] {floating_indicator}"
-          tab_active_fullscreen   "#[bg=cyan,fg=black,bold] [{name}] [] {floating_indicator}"
-          tab_active_sync         "#[bg=cyan,fg=black,bold] [{name}] <> {floating_indicator}"
+          tab_normal              "#[fg=#e0e0ff] [{name}] {floating_indicator}"
+          tab_normal_fullscreen   "#[fg=#e0e0ff] [{name}] [] {floating_indicator}"
+          tab_normal_sync         "#[fg=#e0e0ff] [{name}] <> {floating_indicator}"
+          tab_active              "#[fg=#00ffff,bold,underline] [{name}] {floating_indicator}"
+          tab_active_fullscreen   "#[fg=#00ffff,bold,underline] [{name}] [] {floating_indicator}"
+          tab_active_sync         "#[fg=#00ffff,bold,underline] [{name}] <> {floating_indicator}"
           tab_floating_indicator  "⬚ "
 
-          datetime          "#[bg=cyan,fg=black,bold] [{format}] "
+          datetime          "#[fg=#00ffff,bold] [{format}] "
           datetime_format   "%d/%m/%y %H:%M:%S"
           datetime_timezone "Canada/Toronto"
         }
@@ -52,14 +52,14 @@
             ${''
         plugin location="https://github.com/dj95/zjstatus/releases/download/v0.21.1/zjstatus.wasm" {
           format_left   ""
-          format_center "#[fg=#00ffb3,bg=#0a0a0f]{pipe_zjstatus_hints}"
+          format_center "#[fg=#00ffb3]{pipe_zjstatus_hints}"
           format_right  ""
-          format_space  ""
+          format_space  " "
 
           border_enabled  "false"
           hide_frame_for_single_pane "false"
 
-          pipe_zjstatus_hints_format "#[fg=#ffbe0b,bg=#0a0a0f,bold]{output}"
+          pipe_zjstatus_hints_format "#[fg=#ffbe0b,bold]{output}"
           pipe_zjstatus_hints_rendermode "static"
         }
       ''}
