2576293fd9402c3424f8077cc1d986e2fb2159c1
y0usaf <OA99@Outlook.com>
2026-04-29 22:36:56 -0400

    fix(czkawka): respect configured gtk scale

--- modules/desktop/apps/czkawka.nix
+++ modules/desktop/apps/czkawka.nix
@@ -1,3 +1,25 @@
-{pkgs, ...}: {
-  environment.systemPackages = [pkgs.czkawka];
+{
+  config,
+  pkgs,
+  ...
+}: let
+  inherit (config.user.ui.gtk) scale;
+in {
+  environment.systemPackages = [
+    (
+      if scale == 1.0
+      then pkgs.czkawka
+      else
+        pkgs.symlinkJoin {
+          name = "czkawka-scaled";
+          paths = [pkgs.czkawka];
+          nativeBuildInputs = [pkgs.makeWrapper];
+          postBuild = ''
+            rm $out/bin/krokiet
+            makeWrapper ${pkgs.czkawka}/bin/krokiet $out/bin/krokiet \
+              --set-default SLINT_SCALE_FACTOR ${toString scale}
+          '';
+        }
+    )
+  ];
 }
