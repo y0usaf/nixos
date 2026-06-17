18c0d0301c3f22d29343893f6c93cef44e5f60c9
y0usaf <OA99@Outlook.com>
2026-06-17 13:09:39 -0400

    shell/cat-fetch: bozo -> rudo

--- modules/shell/cat-fetch.nix
+++ modules/shell/cat-fetch.nix
@@ -23,22 +23,22 @@
                 local tomoe_colour="$WALLUST_COLOR13"  # Magenta
                 local moon_colour="$WALLUST_COLOR2"   # Green
                 local ekko_colour="$WALLUST_COLOR12"  # Cyan
-                local bozo_colour="$WALLUST_COLOR9"   # Bright red
+                local rudo_colour="$WALLUST_COLOR9"   # Bright red
               else
                 # Fallback: use basic ANSI colors if Wallust not initialized
                 local tomoe_colour='\033[38;5;13m'    # Bright magenta
                 local moon_colour='\033[38;5;2m'     # Green
                 local ekko_colour='\033[38;5;12m'    # Bright cyan
-                local bozo_colour='\033[38;5;9m'     # Bright red
+                local rudo_colour='\033[38;5;9m'     # Bright red
               fi
 
               local reset='\033[0m'
 
-              echo -e "''${tomoe_colour} ⟋|､      ''${moon_colour}  ⟋|､      ''${ekko_colour}  ⟋|､      ''${bozo_colour}  ⟋|､
-            ''${tomoe_colour}(°､ ｡ 7    ''${moon_colour}(°､ ｡ 7    ''${ekko_colour}(°､ ｡ 7    ''${bozo_colour}(°､ ｡ 7
-            ''${tomoe_colour} |､  ~ヽ   ''${moon_colour} |､  ~ヽ   ''${ekko_colour} |､  ~ヽ   ''${bozo_colour} |､  ~ヽ
-            ''${tomoe_colour} じしf_,)〳''${moon_colour} じしf_,)〳''${ekko_colour} じしf_,)〳''${bozo_colour} じしf_,)〳
-            ''${bozo_colour}  [tomo]   ''${tomoe_colour}  [moon]   ''${moon_colour}  [ekko]   ''${ekko_colour}  [bozo]''${reset}"
+              echo -e "''${tomoe_colour} ⟋|､      ''${moon_colour}  ⟋|､      ''${ekko_colour}  ⟋|､      ''${rudo_colour}  ⟋|､
+            ''${tomoe_colour}(°､ ｡ 7    ''${moon_colour}(°､ ｡ 7    ''${ekko_colour}(°､ ｡ 7    ''${rudo_colour}(°､ ｡ 7
+            ''${tomoe_colour} |､  ~ヽ   ''${moon_colour} |､  ~ヽ   ''${ekko_colour} |､  ~ヽ   ''${rudo_colour} |､  ~ヽ
+            ''${tomoe_colour} じしf_,)〳''${moon_colour} じしf_,)〳''${ekko_colour} じしf_,)〳''${rudo_colour} じしf_,)〳
+            ''${rudo_colour}  [tomo]   ''${tomoe_colour}  [moon]   ''${moon_colour}  [ekko]   ''${ekko_colour}  [rudo]''${reset}"
             }
 
             print_cats
@@ -59,14 +59,14 @@
               let tomoe = $"($e)[38;5;13m"  # Magenta
               let moon = $"($e)[38;5;2m"    # Green
               let ekko = $"($e)[38;5;12m"   # Cyan
-              let bozo = $"($e)[38;5;9m"    # Bright red
+              let rudo = $"($e)[38;5;9m"    # Bright red
               let r = (ansi reset)
 
-              let line1 = $"($tomoe) ⟋|､      ($moon)  ⟋|､      ($ekko)  ⟋|､      ($bozo)  ⟋|､"
-              let line2 = [$tomoe "(°､ ｡ 7    " $moon "(°､ ｡ 7    " $ekko "(°､ ｡ 7    " $bozo "(°､ ｡ 7"] | str join
-              let line3 = $"($tomoe) |､  ~ヽ   ($moon) |､  ~ヽ   ($ekko) |､  ~ヽ   ($bozo) |､  ~ヽ"
-              let line4 = [$tomoe " じしf_,)〳" $moon " じしf_,)〳" $ekko " じしf_,)〳" $bozo " じしf_,)〳"] | str join
-              let line5 = $"($bozo)  [tomo]   ($tomoe)  [moon]   ($moon)  [ekko]   ($ekko)  [bozo]($r)"
+              let line1 = $"($tomoe) ⟋|､      ($moon)  ⟋|､      ($ekko)  ⟋|､      ($rudo)  ⟋|､"
+              let line2 = [$tomoe "(°､ ｡ 7    " $moon "(°､ ｡ 7    " $ekko "(°､ ｡ 7    " $rudo "(°､ ｡ 7"] | str join
+              let line3 = $"($tomoe) |､  ~ヽ   ($moon) |､  ~ヽ   ($ekko) |､  ~ヽ   ($rudo) |､  ~ヽ"
+              let line4 = [$tomoe " じしf_,)〳" $moon " じしf_,)〳" $ekko " じしf_,)〳" $rudo " じしf_,)〳"] | str join
+              let line5 = $"($rudo)  [tomo]   ($tomoe)  [moon]   ($moon)  [ekko]   ($ekko)  [rudo]($r)"
 
               print ([$line1 $line2 $line3 $line4 $line5] | str join "\n")
             }
